4405beb134f1e535c891053a0e948e3d50a024a8
tannuiscoding <tannuchoudhary2000@gmail.com>
2024-10-24 15:55:37 +0530

    deleted venv files

--- package/display.py
+++ package/display.py
@@ -4,6 +4,5 @@ from package.write import plot
 def write(snippet_name, password):
     return plot(snippet_name, password)
     
-
 def show(snippet_name, password, clipboard=None):
     return display(snippet_name, password, clipboard=None)
